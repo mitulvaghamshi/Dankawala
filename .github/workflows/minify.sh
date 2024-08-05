91fb5559abccfc34c5b823dfb70420a7917e9a39
mitulvaghamshi <mitulvaghmashi@gmail.com>
2024-08-05 18:29:22 -0400

    Update minify script

--- .github/workflows/minify.sh
+++
@@ -1,8 +0,0 @@
-cat index.html | while read line; do echo -n "$line" >> temp.html; done;
-mv -f temp.html index.html
-
-cat script.js | while read line; do echo -n "$line" >> temp.js; done;
-mv -f temp.js script.js
-
-cat style.css | while read line; do echo -n "$line" >> temp.css; done;
-mv -f temp.css style.css
